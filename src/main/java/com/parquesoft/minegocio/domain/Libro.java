968cb8a5c1d8a5018d572264876b3b5e04550f0b
Johan Zuleta <johandanielz@gmail.com>
2026-08-15 19:00:04 -0500

    Getters y Setters y constructor de la clase Libro

--- src/main/java/com/parquesoft/minegocio/domain/Libro.java
+++ src/main/java/com/parquesoft/minegocio/domain/Libro.java
@@ -37,4 +37,72 @@ public class Libro {
 	@Column(name = "fecha_registro", nullable = false, updatable = false, insertable = false)
 	private LocalDateTime fechaRegistro;
 	
+	public Libro() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public Integer getAnioPublicacion() {
+		return anioPublicacion;
+	}
+
+	public void setAnioPublicacion(Integer anioPublicacion) {
+		this.anioPublicacion = anioPublicacion;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public boolean isDisponible() {
+		return disponible;
+	}
+
+	public void setDisponible(boolean disponible) {
+		this.disponible = disponible;
+	}
+
+	public LocalDateTime getFechaRegistro() {
+		return fechaRegistro;
+	}
+
+	public void setFechaRegistro(LocalDateTime fechaRegistro) {
+		this.fechaRegistro = fechaRegistro;
+	}
+	
 }
