8141c9471b39d1b4f184470c9ab2a7ceb5e967d5
Johan Zuleta <johandanielz@gmail.com>
2026-08-15 19:07:37 -0500

    Interfaz de la capa de implementación para la clase Libro

---
+++ src/main/java/com/parquesoft/minegocio/service/LibroService.java
@@ -0,0 +1,15 @@
+package com.parquesoft.minegocio.service;
+
+import java.util.List;
+
+import com.parquesoft.minegocio.domain.Libro;
+
+public interface LibroService {
+	
+	List<Libro> listartodos();
+	Libro buscarPorId(Long id);
+	Libro crear(Libro libro);
+	Libro actualizar(Long id, Libro libro);
+	void eliminar(Long id);
+	
+}
