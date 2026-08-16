8d6887929044a9e120f49baba0eee4e809e713b4
Johan Zuleta <johandanielz@gmail.com>
2026-08-16 00:57:50 -0500

    Se crea el EndPoint para crear nuevos libros}

--- src/main/java/com/parquesoft/minegocio/controller/LibroController.java
+++ src/main/java/com/parquesoft/minegocio/controller/LibroController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +32,12 @@ public class LibroController {
 	@GetMapping("/{id}")
 	public Libro obtenerPorId(@PathVariable Long id) {
 		return libroService.buscarPorId(id);
+	}
+	
+	@PostMapping
+	public Libro crear(@RequestBody Libro libro) {
+		Libro creado = libroService.crear(libro);
+		return creado;
 	}
 	
 }
